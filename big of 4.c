b997fd7547004c1a743ffa1c9f5d4643ef6ae044
Pranathi-pamu <chinnupamu11@gmail.com>
2024-10-14 15:25:50 +0530

    Add files via upload

---
+++ big of 4.c
@@ -0,0 +1,44 @@
+#include <math.h>
+
+int main() 
+{
+     int a,b,c,d;
+     printf("enter the 4 value ");
+     scanf("%d %d %d %d",&a,&b,&c,&d);
+     if((a>b)&&(a>c)&&(a>d))
+     printf("%d a is big");
+     else if((b>a)&&(b>c)&&(b>d))
+     printf("%d b is big");
+     else if((c>a)&&(c>b)&&(c>d))
+     printf("%d c is big");
+     else 
+     printf("d is big");
+     
+     
+     
+     
+    
+  
+ 
+       
+         
+       
+         
+       
+       
+     
+     
+      
+      
+      
+      
+      
+     
+     
+      
+ }    
+ 
+     
+
+
+    
